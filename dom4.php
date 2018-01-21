f243ed135c2462679d6b48347272f84c122d95df
Alexandr <alysko@list.ru>
2018-01-21 07:56:23 +0300

    21-01-18

---
+++ dom4.php
@@ -0,0 +1,28 @@
+﻿
+<!--/*Написать функцию, которая обрабатывает данные из формы, проверяет isset(), убирает пробелы, теги и возвращает обработанные данные*/-->
+
+
+<form method="POST">
+Логин: <input type="text" name="login" /><br><br>
+Пароль: <input type="text" name="password" /><br><br>
+<input type="submit" value="Войти">
+</form>
+
+
+<?php
+
+if(isset($_POST['login']) && isset($_POST['password'])){
+	
+	$login= htmlentities($_POST['login']);
+	strip_tags($login);
+	addslashes($login);
+	trim($login);
+	
+    $password = htmlentities($_POST['password']);
+	strip_tags($password);
+	addslashes($password);
+	trim($password);
+    echo "Ваш логин: $login <br> Ваш пароль: $password";
+
+} 
+?>
